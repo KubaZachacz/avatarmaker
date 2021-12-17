6222d089c94106aeb8b89a31a80f3056bc8000c6
Jakub Zachacz <jakub.zachacz@goodylabs.com>
2021-12-17 15:10:33 +0100

    Updates

---
+++ precache-manifest.f311b5890234efb095e1e669f0a05ae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84bde57da901cdf31bdb0c671050426",
+    "url": "/avatarmaker/index.html"
+  },
+  {
+    "revision": "2a9d2906c95bce3b307b",
+    "url": "/avatarmaker/static/css/main.49301103.chunk.css"
+  },
+  {
+    "revision": "41e3b443891d02612bbb",
+    "url": "/avatarmaker/static/js/2.e98bc4f8.chunk.js"
+  },
+  {
+    "revision": "dbfc6432a06a0f194ace1fe336dc2b7c",
+    "url": "/avatarmaker/static/js/2.e98bc4f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a9d2906c95bce3b307b",
+    "url": "/avatarmaker/static/js/main.c75039ba.chunk.js"
+  },
+  {
+    "revision": "5ee029509b4af5be5237",
+    "url": "/avatarmaker/static/js/runtime-main.67e3ce8c.js"
+  }
+]);
